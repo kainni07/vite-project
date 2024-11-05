5f7ef7cf46d39dfe561cd72ba71afdbbded1c371
kainni07 <kainnicapri@gmail.com>
2024-11-05 16:41:19 +0900

    4-15 Styled JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { InlineStyle } from "./components/InlineStyle";
 import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
 
 function App() {
   return (
     <div className='App'>
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
